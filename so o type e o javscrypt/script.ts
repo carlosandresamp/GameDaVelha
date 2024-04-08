f6f91a5ec33dbb14cacd692ddcedfca26010a17f
Carlos André <149840865+carlosandresamp@users.noreply.github.com>
2024-04-08 20:21:01 -0300

    Update script.ts

--- so o type e o javscrypt/script.ts
+++ so o type e o javscrypt/script.ts
@@ -32,9 +32,9 @@ class JogoDaVelha {
 
     private verificarVitoria(): boolean {
         const combinacoesVitoria: number[][] = [
-            [0, 1, 2], [], [], // Linhas
-            [0, 3, 6], [], [], // Colunas
-            [], [] // Diagonais
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],   // Linhas
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
+            [0, 4, 8], [2, 4, 6], // Diagonais
         ];
 
         for (const combinacao of combinacoesVitoria) {
